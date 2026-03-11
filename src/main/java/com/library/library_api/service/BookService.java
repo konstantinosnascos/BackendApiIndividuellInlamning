a545264bb165823a9b82a0b4f962499572745e63
kostas900402 <nascoskostas@gmail.com>
2026-03-11 11:13:38 +0100

    bookservice med createbook, getallbooks, toresponse, getbookbyid

---
+++ src/main/java/com/library/library_api/service/BookService.java
@@ -0,0 +1,54 @@
+package com.library.library_api.service;
+
+
+import com.library.library_api.dto.BookRequest;
+import com.library.library_api.dto.BookResponse;
+import com.library.library_api.exception.BookNotFoundException;
+import com.library.library_api.model.Book;
+import com.library.library_api.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public BookResponse createBook(BookRequest bookRequest) {
+        Book book = new Book(
+                bookRequest.getTitle(),
+                bookRequest.getAuthor(),
+                bookRequest.getIsbn(),
+                bookRequest.getPublishedYear());
+
+        Book savedBook = bookRepository.save(book);
+        return toResponse(savedBook);
+    }
+
+    public List<BookResponse> getAllBooks() {
+        return bookRepository.findAll()
+                .stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    private BookResponse toResponse(Book book){
+        return new BookResponse(
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getIsbn(),
+                book.getPublishedYear());
+    }
+
+    public BookResponse getBookById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+        return toResponse(book);
+    }
+}
