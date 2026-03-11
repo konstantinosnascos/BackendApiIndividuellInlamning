16218c16b612842dd8ad8465798badec0afd41a1
kostas900402 <nascoskostas@gmail.com>
2026-03-11 11:00:13 +0100

    get, set, konstruktor(och tom konstruktor)

--- src/main/java/com/library/library_api/model/Book.java
+++ src/main/java/com/library/library_api/model/Book.java
@@ -19,4 +19,53 @@ public class Book {
     private String isbn;
 
     private Integer publishedYear;
+
+    public Book() {}
+
+    public Book(String title, String author, String isbn, Integer publishedYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedYear = publishedYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(Integer publishedYear) {
+        this.publishedYear = publishedYear;
+    }
 }
