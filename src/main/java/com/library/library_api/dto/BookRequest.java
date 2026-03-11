8817795161bc4ca22e3c742c6fb6e6e4b407f37d
kostas900402 <nascoskostas@gmail.com>
2026-03-11 11:03:47 +0100

    bookrequest, dto in

---
+++ src/main/java/com/library/library_api/dto/BookRequest.java
@@ -0,0 +1,56 @@
+package com.library.library_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class BookRequest {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    private String isbn;
+    private Integer publishedYear;
+
+    public BookRequest() {}
+
+    public BookRequest(String title, String author, String isbn, Integer publishedYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedYear = publishedYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(Integer publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+}
