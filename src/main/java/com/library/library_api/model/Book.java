f81dfc7df8edb296b8aefa3eebe3fce4bfcfe05a
kostas900402 <nascoskostas@gmail.com>
2026-03-11 10:58:30 +0100

    book entity. Integer ist för int, när jag testade förra gången fastnade jag på att int inte kunde va null men integer kan(ingen aning varför det va så)

---
+++ src/main/java/com/library/library_api/model/Book.java
@@ -0,0 +1,22 @@
+package com.library.library_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    private String isbn;
+
+    private Integer publishedYear;
+}
