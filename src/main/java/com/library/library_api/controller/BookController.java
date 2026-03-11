a80fb6c93ad970e5f40751febd98c706f438cbe0
kostas900402 <nascoskostas@gmail.com>
2026-03-11 11:20:13 +0100

    bookcontroller med createbook, getallbooks, getbookbyid

---
+++ src/main/java/com/library/library_api/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.library.library_api.controller;
+
+import com.library.library_api.dto.BookRequest;
+import com.library.library_api.dto.BookResponse;
+import com.library.library_api.service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BookResponse> createBook(
+            @Valid
+            @RequestBody BookRequest bookRequest) {
+        BookResponse bookResponse = bookService.createBook(bookRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookResponse>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponse> getBookById(
+            @PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+}
