56595a176e88c3209e3de30c7a747a90ffee77c5
kostas900402 <nascoskostas@gmail.com>
2026-03-12 10:25:28 +0100

    GlobalExceptionHandler hanterar fel som tänkt. Är ikapp där jag va innan jag insåg att jag inte hade kopplat git och inte hade några commits. Och record-klasser används korrekt.

--- src/main/java/com/library/library_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/library_api/exception/GlobalExceptionHandler.java
@@ -1,19 +1,36 @@
 package com.library.library_api.exception;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(BookNotFoundException.class)
-//    public ResponseEntity<ErrorResponse> handleBookNotFound(
-//            BookNotFoundException ex, HttpServletRequest request) {
-//
-//        ErrorResponse errorResponse = new ErrorResponse(404, "Not Found", ex.getMessage(), request.getRequestURI());
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-//    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleBookNotFound(
+            BookNotFoundException ex, HttpServletRequest request) {
+
+        ErrorResponse errorResponse = new ErrorResponse(404, "Not Found", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(
+            MethodArgumentNotValidException ex, HttpServletRequest request) {
+        String message = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                400, "Bad Request", message, request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 }
