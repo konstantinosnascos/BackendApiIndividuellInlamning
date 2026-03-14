126586ae07666e45cdc4ffd2d96d609e4fe4170d
kostas900402 <nascoskostas@gmail.com>
2026-03-14 11:05:29 +0100

    swagger annotations i BookController

--- src/main/java/com/library/library_api/controller/v1/BookController.java
+++ src/main/java/com/library/library_api/controller/v1/BookController.java
@@ -3,6 +3,9 @@ package com.library.library_api.controller.v1;
 import com.library.library_api.dto.v1.BookRequest;
 import com.library.library_api.dto.v1.BookResponse;
 import com.library.library_api.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,11 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(summary = "Create a new book", description = "Create a new book and return that book")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "book creation successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @PostMapping
     public ResponseEntity<BookResponse> createBook(
             @Valid
@@ -27,11 +35,18 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.CREATED).body(bookResponse);
     }
 
+    @Operation(summary = "Get all books", description = "Return a list of all books")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of all books")
     @GetMapping
     public ResponseEntity<List<BookResponse>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
     }
 
+    @Operation(summary = "Get book by ID", description = "Returns a book by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Book found"),
+            @ApiResponse(responseCode = "404", description = "Book not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<BookResponse> getBookById(
             @PathVariable Long id) {
