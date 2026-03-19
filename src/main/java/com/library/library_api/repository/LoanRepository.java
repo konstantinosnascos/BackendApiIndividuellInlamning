3356f607b8f7e9759a9da8196f2503b542db850f
kostas900402 <nascoskostas@gmail.com>
2026-03-19 08:02:21 +0100

    missade @Repository i i loanrepo klass. lagt till en metod också, findByBookIdAndReturnDateIsNull(Long bookId)

--- src/main/java/com/library/library_api/repository/LoanRepository.java
+++ src/main/java/com/library/library_api/repository/LoanRepository.java
@@ -2,6 +2,11 @@ package com.library.library_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.library.library_api.model.Loan;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    Optional<Loan> findByBookIdAndReturnDateIsNull(Long bookId);
 }
