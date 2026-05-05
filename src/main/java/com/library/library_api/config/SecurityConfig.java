f4c4c5013990d25957bad02a78b5fc40a8fb672d
kostas900402 <nascoskostas@gmail.com>
2026-05-05 10:13:49 +0200

    hämtat in securityConfig från annan branch jag testade i ett par veckor sen. om jag förstår uppgiften rätt så ska det ändras en hel del där i. Hämtade samtidigt in app.properties, men tog bort 2 raderna jag lagt till där för dom används inte. Ska nog börja med att göra klart CRUD(inte någon delete som krav tidigare), också put(update) tills nu har jag endast använt patch för att uppdatera returnerat lån.

--- src/main/java/com/library/library_api/config/SecurityConfig.java
+++ src/main/java/com/library/library_api/config/SecurityConfig.java
@@ -15,44 +15,44 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers(
-                        "/h2-console/**",
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                ).permitAll()
-                        .requestMatchers("/api/v1/books/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
-
-        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user= User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("admin123"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth.requestMatchers(
+//                        "/h2-console/**",
+//                        "/v3/api-docs/**",
+//                        "/swagger-ui/**",
+//                        "/swagger-ui.html"
+//                ).permitAll()
+//                        .requestMatchers("/api/v1/books/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults());
+//
+//        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+//        UserDetails user= User.builder()
+//                .username("user")
+//                .password(passwordEncoder.encode("password"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder.encode("admin123"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
