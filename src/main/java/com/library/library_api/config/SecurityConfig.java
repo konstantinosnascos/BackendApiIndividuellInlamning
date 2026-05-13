4ff783c02dfb9dd4404efdb512b3824795ad477e
kostas900402 <nascoskostas@gmail.com>
2026-05-13 09:35:44 +0200

    om jag förstår cors-policy rätt så ska man bara kunna nå backend från bestämd url nu, men inga tester eller frontend än så vet ej om det fungerar som tänkt än

--- src/main/java/com/library/library_api/config/SecurityConfig.java
+++ src/main/java/com/library/library_api/config/SecurityConfig.java
@@ -13,13 +13,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers(
                         "/h2-console/**",
                         "/v3/api-docs/**",
@@ -43,6 +50,17 @@ public class SecurityConfig {
                         "/api/v1/authors/**",
                         "/api/v1/loans/**"
                             ).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT,
+                        "/api/v1/books/**",
+                        "/api/v1/authors/**",
+                        "/api/v1/loans/**"
+                ).hasAnyRole("USER", "ADMIN")
+
+                .requestMatchers(HttpMethod.PATCH,
+                        "/api/v1/books/**",
+                        "/api/v1/authors/**",
+                        "/api/v1/loans/**"
+                ).hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
@@ -71,5 +89,29 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:3000"));
+
+        configuration.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+
+        configuration.setAllowedHeaders(
+                List.of("*"));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/api/**",
+                configuration);
+
+        return source;
     }
 }
